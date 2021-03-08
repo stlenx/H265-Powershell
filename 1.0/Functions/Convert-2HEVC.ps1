d998dccf46230c3de55c0a65785d058ff516da7c
ixi-your-face <ixi@ixi.me.uk>
2021-03-08 12:12:47 +0000

    fixing tabulated vs spaces. Hint: Tabs win.

--- 1.0/Functions/Convert-2HEVC.ps1
+++ 1.0/Functions/Convert-2HEVC.ps1
@@ -1,65 +1,65 @@
 function Convert-2HEVC {
-    param (
-        [alias('d')]
-        $Dir,
-        [alias('p')]
-        $Preset
-    )
+	param (
+		[alias('d')]
+		$Dir,
+		[alias('p')]
+		$Preset
+	)
 
-    Begin {
+	Begin {
 
-        # Config lists
+		# Config lists
 
-        $ValidPresets = (
-            "fast",
-            "medium",
-            "slow"
-        )
+		$ValidPresets = (
+			"fast",
+			"medium",
+			"slow"
+		)
 
-        $formats = (
+		$formats = (
 			'mp4',
 			'mkv'
-        )
+		)
 
-        # Clearing the terminal becuase this makes shit tidy.
-        Clear-Host
+		# Clearing the terminal becuase this makes shit tidy.
+		Clear-Host
 
-        while(!($ValidPresets -contains $Preset)) {
-            if($Preset) {
-                write-Host -BackgroundColor Black -ForegroundColor Red "Preset of: '$Preset' is not valid. Please select a valid Preset."
-            }
-            $Preset = (read-host "Please input a valid option (fast, medium or slow)")
-        }
-        
+		while(!($ValidPresets -contains $Preset)) {
+			if($Preset) {
+				write-Host -BackgroundColor Black -ForegroundColor Red "Preset of: '$Preset' is not valid. Please select a valid Preset."
+			}
+			$Preset = (read-host "Please input a valid option (fast, medium or slow)")
+		}
+		
 		$UnformatDir = $Dir
 
-        # Okay I'm less angry about this now. But still fuck you for adding [] to dir names.
-        if($Dir -Match "\[|\]"){
-            $Dir = $Dir.Replace('[', '``[')
-            $Dir = $Dir.Replace(']', '``]')
-        }
-        
-        if(!(test-path $Dir)){
-            Write-Host -ForegroundColor Red -BackgroundColor Black "Path $Dir is not valid. Please input a valid path and try again."
-            break #69
-        }
+		# Okay I'm less angry about this now. But still fuck you for adding [] to dir names.
+		if($Dir -Match "\[|\]"){
+			$Dir = $Dir.Replace('[', '``[')
+			$Dir = $Dir.Replace(']', '``]')
+		}
 		
-        # Chage to the Dir because running get-childitem is fucked.
+		if(!(test-path $Dir)){
+			Write-Host -ForegroundColor Red -BackgroundColor Black "Path $Dir is not valid. Please input a valid path and try again."
+			break #69
+		}
+		
+		# Chage to the Dir because running get-childitem is fucked.
 		cd $Dir
-    }
+	}
 
-    Process{
-        if(!(test-path $UnformatDir\Converted)) {
-            new-item $UnformatDir\Converted -itemtype Directory
-        }
+	Process{
+		if(!(test-path $UnformatDir\Converted)) {
+			new-item $UnformatDir\Converted -itemtype Directory
+		}
 
-        foreach ($File in (Get-ChildItem)) {
-            if($formats -contains ($File.name.split('.')[-1])) {
-		        $Name = $File.name
-                $input = "$UnformatDir\$Name"
-                $output = "$UnformatDir\Converted\$Name"
-                ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $input -map 0 -c copy -c:v hevc_nvenc -Preset $Preset $output
-            }
-        }
-    }
+		foreach ($File in (Get-ChildItem)) {
+			if($formats -contains ($File.name.split('.')[-1])) {
+				$Name = $File.name
+				$input = "$UnformatDir\$Name"
+				$output = "$UnformatDir\Converted\$Name"
+				ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $input -map 0 -c copy -c:v hevc_nvenc -Preset $Preset $output
+			}
+		}
+	}
 }
